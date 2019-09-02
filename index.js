015e2edac8fb7d88f5f51308a66508da23fca7ba
Maycon Silva <maycon.mao@hotmail.com>
2019-09-01 22:04:40 -0300

    chore: Desafio_01 Conceitos de NodeJS

---
+++ index.js
@@ -0,0 +1,84 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+let request = 0;
+const projects = [];
+
+function checkProject(req, res, next) {
+  const { id } = req.params;
+  const projects = projects.find(p => p.id == id);
+
+  if (!project) {
+    return res.status(400).json({ error: 'Project not found' });
+  }
+
+  return next();
+}
+
+function logRequests(req, res, next) {
+  request++;
+
+  console.log(`Número de requisições: ${request}`);
+
+  return next();
+}
+
+server.use(logRequests);
+
+server.get('/projects', (req, res) => {
+  return res.json(projects);
+})
+
+server.post('/projects', (req, res) => {
+
+  const { id, title } = req.body;
+
+  const project = {
+    id,
+    title,
+    tasks: []
+  }
+  projects.push(project);
+
+  return res.json(project);
+});
+
+server.put('/projects/:id', checkProject, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.title = title;
+
+  return res.json(project);
+});
+
+server.delete('/projects/:id', checkProject, (req, res) => {
+  const { id } = req.params;
+
+  const projectIndex = projects.findIndex(p => p.id == id);
+
+  projects.splice(projectIndex, 1);
+
+  return res.send();
+})
+
+server.post('/projects/:id/tasks', checkProject, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.tasks.push(title);
+
+  return res.json(project);
+});
+
+
+
+
+server.listen(3000);
